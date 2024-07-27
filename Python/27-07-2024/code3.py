b1a1d83bc99698e5ebd8cc5cc65e5753f0052925
pvigneswari <Vigneswari.Pappu@informationtechconsultants.co.uk>
2024-07-27 21:43:23 +0100

    new codes

---
+++ Python/27-07-2024/code3.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_ab = a + b
+    difference_ab = a - b
+    product_ab = a * b
+    
+    print(sum_ab)
+    print(difference_ab)
+    print(product_ab)
---
+++ Python/27-07-2024/code4.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    integer_division_ab = a // b
+    float_division_ab = a / b
+    
+    print(integer_division_ab)
+    print(float_division_ab)

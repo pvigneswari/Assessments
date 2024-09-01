6fbdc59a8ede3af6ec2e48e8b9c46dafed4575db
pvigneswari <Vigneswari.Pappu@informationtechconsultants.co.uk>
2024-09-01 16:58:54 +0100

    testing started

--- Project/viguTesting.py
+++ Project/viguTesting.py
@@ -1,1 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import sys
+
 print('This is running from jenkings')
+
+# Initialize Spark Session
+spark = SparkSession.builder \
+    .appName("Data_Cleaning_Python") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+print('This is running from jenkings spark working good')
+
+# Stop the Spark session
+spark.stop()
